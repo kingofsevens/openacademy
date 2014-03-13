189faa8026b5039b4459abe99133ae0d534a2afc
Erdem Uney <erdem@7gates.co>
2014-03-13 19:12:24 +0200

    [ADD] ex.6.1. function field to compute the percentage of taken seats in sessions

--- session.py
+++ session.py
@@ -9,7 +9,16 @@ class Session(osv.Model):
     _name = 'openacademy.session'
     _description = 'Sessions'
     _order = 'start_date'
-    
+
+    def _get_completion(self, cr, uid, ids, field, arg, context=None):
+        res = {}
+        for session in self.browse(cr, uid, ids, context):
+            if session.seats:
+                res[session.id] = 100.0 * len(session.attendees) / session.seats
+            else:
+                res[session.id] = 0.0
+        return res
+
     _columns = {
         'name': fields.char(string='Name', required=True),
         'start_date': fields.date(string='Start Date'),
@@ -19,10 +28,15 @@ class Session(osv.Model):
         
         # relational fields
         'course': fields.many2one('openacademy.course', 
-                # ’cascade’ will destroy session if course_id was deleted
-                ondelete='cascade', string='Course', required=True),
+                        # ’cascade’ will destroy session if course_id was deleted
+                        ondelete='cascade', string='Course', required=True),
         'instructor': fields.many2one('res.partner', string='Instructor',
-                            domain=['|', ('instructor', '=', True),
-                                    ('category_id.name', 'like', 'Teacher')]),
+                        domain=['|', ('instructor', '=', True),
+                                ('category_id.name', 'like', 'Teacher')]),
         'attendees': fields.many2many('res.partner', string='Attendees'),
-    }
+        
+        # function fields
+        'completion': fields.function(_get_completion, type='float',
+                        string='Completion', help='Percentage of taken seats.'),
+        
+        }
