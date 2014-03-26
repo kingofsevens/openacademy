bbd2239c7e6e21bd8886db2c763d3cf7739ac675
Erdem Uney <erdem@7gates.co>
2014-03-26 17:13:08 +0200

    [IMP] ex.10.5. make the wizard available as a contextual action

--- wizard/subscribe.py
+++ wizard/subscribe.py
@@ -14,6 +14,16 @@ class Subscribe(osv.TransientModel):
         'attendees': fields.many2many('res.partner', string='Attendees'),
     }
 
+    def _get_default_session(self, cr, uid, context=None):
+        return context and \
+            context.get('active_model') == 'openacademy.session' and \
+            context.get('active_id') or False
+
+    _defaults = {
+        'session': _get_default_session,
+    }
+
+
     def action_subscribe(self, cr, uid, ids, context=None):
         # we know that ids is a list with one element only
         wizard = self.browse(cr, uid, ids[0], context)
